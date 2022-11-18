918252d634f829a09e25d3329e54a0d8767e517b
James Halim <tirtajames45@gmail.com>
2022-11-18 13:13:32 +0700

    new

---
+++ .local/share/theme/colors.h
@@ -0,0 +1,6 @@
+static char normbordercolor[]       = "#222222";
+static char normfgcolor[]           = "#dad2c8";
+static char normbgcolor[]           = "#222222";
+static char selbordercolor[]        = "#988165";
+static char selbgcolor[]            = "#988165";
+static char selfgcolor[]            = "#222222";
