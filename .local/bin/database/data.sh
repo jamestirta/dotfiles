cc95e9faa2063efd56bca7072a6b701dde74b5e7
James Halim <tirtajames45@gmail.com>
2023-01-01 00:03:55 +0700

    new

--- .local/bin/database/data.sh
+++ .local/bin/database/data.sh
@@ -5,18 +5,15 @@ DATA_DIR=$HOME/.local/bin/database/
 files=$(find "$DATA_DIR" -type f ! -name '*.sh')
 # vars=$(for file in $files; do head -n1 "$file"; done)
 vars=($(for file in $files; do head -n1 "$file"; done))
-whereVar(){ 
-	i=1
+whereVar(){ i=1
 	for var in "${vars[@]}"; do
 		[ "$var" = "$1" ] && echo "$i" && return
 		i=$((i=i+1))
 	done;}
-loopFile(){
-	for file in $files; do
+loopFile(){ for file in $files; do
 		"$@" "$file"
 	done;}
-whichGet(){
-	loopFile awk -v which="$1" 'NF {print $which}' | tail -n+2;}
+whichGet(){ loopFile awk -v which="$1" 'NF {print $which}' | tail -n+2;}
 getDate(){ whichGet "$(whereVar date)";}
 getItem(){ whichGet "$(whereVar item)";}
 getCost(){ whichGet "$(whereVar cost)";}
@@ -28,4 +25,4 @@ whereDate(){ whereWhich date "$1";}
 whereItem(){ whereWhich item "$1";}
 whereCost(){ whereWhich cost "$1";}
 # appendYear(){ awk '{print $1"-2022",$2,$3}'
-skipFirst(){ tail -n+2;}
+# skipFirst(){ tail -n+2;}
